7afccdc17d4ef9da458e1d7ca3887652040e9957
PNAM <tpnam2014@gmail.com>
2024-02-26 21:36:04 +0700

    convert

--- spin/urls.py
+++ spin/urls.py
@@ -6,5 +6,5 @@ app_name = 'spin'
 urlpatterns = [
     path("", views.navigation, name="navigation"),
     path('spin_form/', views.spin_form, name='spin_form'),
-    
+    path('spin_wheel/<str:spin_code>/', views.spin_wheel, name='spin_wheel'),
 ]
--- spin/views.py
+++ spin/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import SpinCodeForm
 from .models import SpinCode, UsedSpinCode
 from django.contrib import messages 
+from django.urls import reverse
 
 def spin_form(request):
     if request.method == 'POST':
@@ -13,13 +14,19 @@ def spin_form(request):
                 return render(request, 'spin/spin_form.html', {'form': form})
             elif SpinCode.objects.filter(code=spin_code).exists():
                 UsedSpinCode.objects.create(code=spin_code)
-                return render(request, 'spin/spin_wheel.html', {'spin_code': spin_code})
+                return redirect(reverse('spin:spin_wheel', kwargs={'spin_code': spin_code}))
+                # return render(request, 'spin/spin_wheel.html', {'spin_code': spin_code})
             else:
                 messages.error(request, 'Spin code không hợp lệ')
                 return render(request, 'spin/spin_form.html', {'form': form})
     else:
         form = SpinCodeForm()
     return render(request, 'spin/spin_form.html', {'form': form})
+
+def spin_wheel(request, spin_code):
+    # Xử lý logic của trang spin_wheel ở đây
+    return render(request, 'spin/spin_wheel.html', {'spin_code': spin_code})
+
 
 
 def navigation(request):
