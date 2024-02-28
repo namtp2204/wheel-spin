46b7f05b22601ea10da57f63b783c43a048a082a
PNAM <tpnam2014@gmail.com>
2024-02-28 10:36:37 +0700

    csrf

--- spin/views.py
+++ spin/views.py
@@ -3,7 +3,9 @@ from .forms import SpinCodeForm
 from .models import SpinCode, UsedSpinCode
 from django.contrib import messages 
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_protect
 
+@csrf_protect
 def spin_form(request):
     if request.method == 'POST':
         form = SpinCodeForm(request.POST)
@@ -14,7 +16,6 @@ def spin_form(request):
                 return render(request, 'spin/spin_form.html', {'form': form})
             elif SpinCode.objects.filter(code=spin_code).exists():
                 UsedSpinCode.objects.create(code=spin_code)
-                # return redirect(reverse('spin:spin_wheel', kwargs={'spin_code': spin_code}))
                 return redirect('spin_wheel', spin_code=spin_code)
             else:
                 messages.error(request, 'Spin code không hợp lệ')
@@ -23,6 +24,7 @@ def spin_form(request):
         form = SpinCodeForm()
     return render(request, 'spin/spin_form.html', {'form': form})
 
+@csrf_protect
 def spin_wheel(request, spin_code):
     return render(request, 'spin/spin_wheel.html', {'spin_code': spin_code})
 
